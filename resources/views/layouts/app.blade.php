cdd0fae8c2c8ead3a131d1e99c667d36a2054798
mehran nikoobayan <nikoomarch@gmail.com>
2019-05-08 07:37:50 +0430

    configure for https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="rtl">
     <div id="app">
@@ -68,8 +68,8 @@
             @yield('content')
         </main>
     </div>
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{asset('js/sweetalert2.js')}}"></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    <script src="{{secure_asset('js/sweetalert2.js')}}"></script>
     @yield('script')
 </body>
 </html>
